4815e10fb17f74c66cf35272fb6e1d3bd6dc35c1
Leo Godin <leogodin217@gmail.com>
2010-01-21 22:19:35 -0700

    Changed ruby to source instead of apt

--- setup.sh
+++ setup.sh
@@ -1,13 +1,23 @@
-aptitude install -y vim build-essential g++ sqlite3 libsqlite3-dev libxslt-dev libxml2 libxml2-dev  ruby1.9  ruby1.9-dev rdoc1.9 irb1.9 git gitk subversion
+# Install the basics
+aptitude install -y vim build-essential g++ sqlite3 libsqlite3-dev libxslt-dev libxml2 libxml2-dev  git gitk subversion
 
-ln -s /usr/bin/ruby1.9 /usr/bin/ruby
-ln -s /usr/bin/erb1.9 /usr/bin/erb
-ln -s /usr/bin/rake1.9 /usr/bin/rake
-ln -s /usr/bin/rdoc1.9 /usr/bin/rdoc
-ln -s /usr/bin/testrb1.9 /usr/bin/testrb1.9
-
+# Setup environment
 cp /usr/share/vim/vim72/vimrc_example.vim ~leo/.vimrc
 chown leo:leo ~leo/.vimrc
 echo 'set tabstop=4' >> ~leo/.vimrc
 
-cp -rp .ssh ~
+# Setup ruby
+wget ftp://ftp.ruby-lang.org/pub/ruby/1.9/ruby-1.9.1-p376.tar.gz
+gunzip ruby-1.9.1-p376.tar.gz
+cd ruby-1.9.1-p376.tar.gz
+./configure
+make
+make install
+cd ..
+
+# Setup ruby gems
+wget http://rubyforge.org/frs/download.php/60718/rubygems-1.3.5.tgz
+tar -xpzf rubygems-1.3.5.tgz
+cd rubygems-1.3.5
+ruby setup.rb
+gem install rails do_sqlite3 rspec rspec-rails cucumber webrat rails_datamapper paperclip haml factory_girl factory_girl_extensions
--- setupgems.sh
+++ setupgems.sh
@@ -2,4 +2,4 @@ wget http://rubyforge.org/frs/download.php/60718/rubygems-1.3.5.tgz
 tar -xpzf rubygems-1.3.5.tgz
 cd rubygems-1.3.5
 ruby setup.rb
-gem install rails do_sqlite3 rspec rspec-rails cucumber webrat datamapper paperclip haml
+gem install rails do_sqlite3 rspec rspec-rails cucumber webrat rails_datamapper paperclip haml factory_girl factory_girl_extensions
