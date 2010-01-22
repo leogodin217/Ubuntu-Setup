031edae62fdfd1653ab01fa9c0c275852944c055
Leo Godin <leogodin217@gmail.com>
2010-01-22 00:45:32 -0700

    commented out gem install (it's part of ruby now) and modified the ruby instal.

--- setup.sh
+++ setup.sh
@@ -1,5 +1,5 @@
 # Install the basics
-aptitude install -y vim build-essential g++ sqlite3 libsqlite3-dev libxslt-dev libxml2 libxml2-dev  git gitk subversion
+aptitude install -y vim build-essential g++ sqlite3 libsqlite3-dev libxslt-dev libxml2 libxml2-dev  git gitk subversion ssl-dev
 
 # Setup environment
 cp /usr/share/vim/vim72/vimrc_example.vim ~leo/.vimrc
@@ -9,15 +9,18 @@ echo 'set tabstop=4' >> ~leo/.vimrc
 # Setup ruby
 wget ftp://ftp.ruby-lang.org/pub/ruby/1.9/ruby-1.9.1-p376.tar.gz
 gunzip ruby-1.9.1-p376.tar.gz
-cd ruby-1.9.1-p376.tar.gz
+tar -xpf ruby-1.9.1-p376.tar
+cd ruby-1.9.1-p376
 ./configure
 make
 make install
 cd ..
 
 # Setup ruby gems
-wget http://rubyforge.org/frs/download.php/60718/rubygems-1.3.5.tgz
-tar -xpzf rubygems-1.3.5.tgz
-cd rubygems-1.3.5
-ruby setup.rb
-gem install rails do_sqlite3 rspec rspec-rails cucumber webrat rails_datamapper paperclip haml factory_girl factory_girl_extensions
+#wget http://rubyforge.org/frs/download.php/60718/rubygems-1.3.5.tgz
+#tar -xpzf rubygems-1.3.5.tgz
+#cd rubygems-1.3.5
+#ruby setup.rb
+gem install rails do_sqlite3 sqlite3-ruby rspec rspec-rails cucumber webrat rails_datamapper datamapper  paperclip haml factory_girl factory_girl_extensions
+# For 1.2.3 error in rspec
+gem install test-unit -v=1.2.3
